86ef4bc3d04e6d39ea1005a9835acd873b964cd3
gonadr09 <gonzalo_diaz09@hotmail.com>
2022-10-04 17:00:14 -0300

    fix testimony arrows v3

--- js/questions.js
+++ js/questions.js
@@ -5,10 +5,10 @@ const questions = () => {
     question.addEventListener("click", () => {
       let height = 0;
       let answer = question.nextElementSibling;
-      let addPadding = question.parentElement.parentElement;
+      let addPadding = question.parentElement.parentElement;      
 
       addPadding.classList.toggle("questions__padding--add");
-      question.children[0].classList.toggle("questions__arrow--rotate");
+      question.children[1].classList.toggle("questions__arrow--rotate");
 
       if (answer.clientHeight === 0) {
         height = answer.scrollHeight;
